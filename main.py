4e2444347e711a3dccc96d649063640dc4d9f4d5
ChristianTStu <Christiantstu@gmail.com>
2025-03-14 13:09:19 -0500

    add get_num_char function, update import method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 
 def get_book_text(path_to_file):
@@ -10,7 +10,9 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(book_text)
+    char_count = get_num_char(book_text)
     print(f"{word_count} words found in the document")
+    print(char_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,16 @@
 def get_num_words(text):
     return len(text.split())
+
+def get_num_char(text):
+    char_count = {}
+    for char in text:
+        char = char.lower()
+
+        if char in char_count:
+            char_count[char] += 1
+        elif char not in char_count:
+            char_count[char] = 1
+
+    return char_count
+
+            
