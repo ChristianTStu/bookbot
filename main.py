bf6eb0000ac43c37761a7a136c43447942d5c8c1
ChristianTStu <Christiantstu@gmail.com>
2025-03-14 13:54:25 -0500

    add get_sort_dict function, format main.py output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_char
+from stats import get_num_words, get_num_char, get_sort_dict
 
 
 def get_book_text(path_to_file):
@@ -11,8 +11,24 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(book_text)
     char_count = get_num_char(book_text)
-    print(f"{word_count} words found in the document")
-    print(char_count)
+    sorted_chars = get_sort_dict(char_count)
+    
+    # Print the report
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    
+    # Print each character and its count
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        # Only print alphabetical characters
+        if char.isalpha():
+            print(f"{char}: {count}")
+    
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -13,4 +13,15 @@ def get_num_char(text):
 
     return char_count
 
-            
+def get_sort_dict(char_count):
+    char_list = []
+    for char, count in char_count.items():
+        char_dict = {"char": char, "count": count}
+        char_list.append(char_dict)
+
+    def sort_on(dict):
+        return dict["count"]
+    
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
