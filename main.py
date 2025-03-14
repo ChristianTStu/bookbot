61275516abd8811e4e7a1f0dc2ffbc33efdb54bf
ChristianTStu <Christiantstu@gmail.com>
2025-03-14 12:46:26 -0500

    Add main, stats, gitignore

---
+++ main.py
@@ -0,0 +1,16 @@
+from stats import get_num_words
+
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    word_count = get_num_words(book_text)
+    print(f"{word_count} words found in the document")
+
+
+main()
